87f34f7610d5371f1f8a4b883f63635e4ec4b052
liquan.zhou <liquan.zhou@t2mobile.com>
2024-12-06 17:39:57 +0800

    FPS-61 [2]Mini security permissions disable
    * Comment: disable selinux on Mini releases
    * Root Cause: Requirement
    * Solution: Add "androidboot.selinux=permissive" in cmdline
    * Test Steps: Build pass
    * Test Result: Build pass
    
    Change-Id: I877c06300a86423d6dee873b2e4c38093d934c49

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,5 @@
 include device/qcom/volcano/BoardConfig.mk
+include device/fairphone/common/BoardConfigCommon.mk
 
 ifneq ($(wildcard device/fairphone/fps-kernel/extra_cmdline),)
 BOARD_KERNEL_CMDLINE += $(file < device/fairphone/fps-kernel/extra_cmdline)
