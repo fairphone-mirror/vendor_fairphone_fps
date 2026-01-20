24ce576fa4f9b1a85755f9474d8fc31f22e467b3
android-t1 <android-t1@t2mobile.com>
2026-01-20 16:39:51 +0800

    update tpye QREL to rc
    Change-Id: I138f8058c534036294213e505416c7defff3ad12

--- fps.mk
+++ fps.mk
@@ -24,7 +24,7 @@ PRODUCT_BRAND_FOR_ATTESTATION := Fairphone
 PRODUCT_NAME_FOR_ATTESTATION := FP6
 
 ifndef FAIRPHONE_REL_TYPE
-  FAIRPHONE_REL_TYPE := DEV
+  FAIRPHONE_REL_TYPE := QREL
 endif
 ifneq ($(wildcard $(TOPDIR)version/version.ini),)
   FAIRPHONE_BUILD_NUMBER := $(shell awk '/BUILD_AP_VER/ {print $$3}' $(TOPDIR)version/version.ini)
